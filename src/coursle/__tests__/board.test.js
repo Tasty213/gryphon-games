0a7b364de6894aa239fee5c528ff28c660f4a180
Tasty213 <30747514+Tasty213@users.noreply.github.com>
2022-05-15 18:58:07 +0100

    making test env constructor dynamic

--- src/coursle/__tests__/board.test.js
+++ src/coursle/__tests__/board.test.js
@@ -25,15 +25,18 @@ describe('Board class non-static tests', ()=> {
 
 /**
  * Creates a coursle board to test on
+ * @param {string} word
+ * @param {number} numberOfGuesses
+ * @param {number} padding
  * @return {Board}
  */
-function constructCoursleTestEnvironment() {
+function constructCoursleTestEnvironment(word, numberOfGuesses, padding) {
   document.body.innerHTML = $return = `
   <div id="coursleContainer" style="width: 1000px;">
     <span class="coursleLabel"><span class="coursleButton" onclick="submitGuess();">Submit</span></span>
     <span class="coursleLabel"><span class="coursleText" id="coursleMessage">Press the submit button to make a guess.</span></span>
     <div id="coursle"></div>
   </div><br style="clear:both;"/>`;
-  globalThis.NUMBER_OF_GUESSES = 4;
-  return new Board('TEST', 10);
+  globalThis.NUMBER_OF_GUESSES = numberOfGuesses;
+  return new Board(word, padding);
 }
